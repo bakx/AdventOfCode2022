edd8f0534da799d15a913043f0ed045439fb600f
Gideon Bakx <gbakx@kx.com>
2022-12-03 04:29:50 -0700

    Added day3 (not optimized)

---
+++ day3.c
@@ -0,0 +1,113 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool hasCharacter(char * line,  char c);
+
+char findDuplicate(char * comp1, char * comp2)
+{
+    for (int i = 0; i < strlen(comp1); i++)
+    {
+        for (int j = 0; j < strlen(comp2); j++)
+        {
+            if (comp1[i]==comp2[j])
+                return comp1[i];
+        }
+    }
+}
+
+int getCharScore(char item)
+{
+    int ascii = (int) item;
+
+    if (ascii > 96) // a starts at 97
+        return ascii-96;
+    
+    return ascii-38; // A starts at 65 - 26 as A scores as 27
+}
+
+int processRucksacks(char ** rucksacks, int offset)
+{
+    for (int i = offset + 1; i < offset + 3; i++)
+    {
+        for (int j = 0; j < strlen(rucksacks[offset]); j++)
+        {
+            if (hasCharacter(rucksacks[offset + 1], rucksacks[offset][j]) && hasCharacter(rucksacks[offset + 2], rucksacks[offset][j]))
+                return getCharScore(rucksacks[offset][j]);
+        }
+    }
+    return 0;
+}
+
+bool hasCharacter(char * line,  char c)
+{
+    for (int i = 0; i < strlen(line); i++)
+    {
+        if (line[i] == c) return true;
+    }
+
+    return false;
+}
+
+int main(int argc, const char *const argv[])
+{
+    FILE * fp = fopen("day3.txt", "r");
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    long int result = 0;
+    long int badgeResult = 0;
+
+    char * rucksacks[6]; 
+    
+    for (int i = 0; i < 6; i++)
+        rucksacks[i] = "";
+
+    while ((read = getline(&line, &len, fp)) != -1)
+    {
+        char * comp1 = malloc(sizeof(char) * read - 1 / 2) + 1;
+        char * comp2 = malloc(sizeof(char) * read - 1 / 2) + 1;
+
+        for(int i=0; i < read - 1;i++)
+        {
+            if (i < read / 2)
+                comp1[i] = line[i];
+            else
+                comp2[i - read / 2 ] = line[i];
+        }
+
+        comp1 += '\0';
+        comp2 += '\0';
+
+        char dup = findDuplicate(comp1, comp2);
+        result += getCharScore(dup);
+
+        for (int i=0; i < 6; i++)
+        {
+            if (strlen(rucksacks[i]) > 0)
+                continue;
+            
+            rucksacks[i] = strdup(line);
+            break;
+        }
+
+        if (strlen(rucksacks[5]) > 0) { // Process rucksacks
+            badgeResult += processRucksacks(rucksacks, 0);
+            badgeResult += processRucksacks(rucksacks, 3);
+            
+            for (int i = 0; i < 6; i++)
+                rucksacks[i] = "";
+        }
+
+        comp1 = comp2 = "";
+    }
+
+    printf("Total points #1 %ld\n", result);
+    printf("Total points #2 %ld\n", badgeResult);
+    
+    free(line);
+    fclose(fp);
+}
